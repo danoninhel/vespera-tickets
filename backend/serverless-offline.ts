1b75d90f2c0b9c155b957aa31f27b885cc3a2fb3
danonel <dldm13@gmail.com>
2026-04-16 20:42:49 -0300

    chore: remove unused config files (Dockerfile, docker-compose, serverless)

--- backend/serverless-offline.ts
+++
@@ -1,7 +0,0 @@
-import "dotenv/config";
-import { startCron } from "./src/cron.js";
-
-startCron();
-
-const ServerlessOffline = await import("serverless-offline").then((m) => m.default);
-await ServerlessOffline();
